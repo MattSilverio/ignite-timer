019fb8e6fc14139cae3b059d42f07e26678a3fb6
Matheus Silverio <matheusphillipo@gmail.com>
2023-01-22 23:06:07 -0300

    feat: Timer na aba

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -90,6 +90,10 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
 
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${seconds}`;
+  }, [minutes, seconds, activeCycle]);
+
   const task = watch("task"); // Observe o campo task registrado no register
   const isSubmitDisabled = !task;
 
