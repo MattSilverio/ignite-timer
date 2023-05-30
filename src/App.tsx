33a98387c70422b0dcfa05be2972dbd8e51126bf
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-29 22:23:23 -0300

    add: CyclesContext to Home and History routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Routes/Router'
-import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Routes/Router";
+import { BrowserRouter } from "react-router-dom";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
         <BrowserRouter>
-          <Router />
+          <CyclesContextProvider>
+            <Router />
+          </CyclesContextProvider>
         </BrowserRouter>
 
         <GlobalStyle />
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
