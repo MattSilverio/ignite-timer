f023f97c17c47a3508cae765ffc11e5878fea5db
Matheus Silverio <matheusphillipo@gmail.com>
2023-06-09 12:40:42 -0300

    chore: separated interface files

---
+++ src/interfaces/ICycle.ts
@@ -0,0 +1,29 @@
+export interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
+
+export interface CyclesState {
+  cycles: Cycle[];
+  activeCycleId: string | null;
+}
+
+export interface CreateCycleData {
+  task: string;
+  minutesAmount: number;
+}
+
+export interface CyclesContextData {
+  cycles: Cycle[];
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  amountSecondsPassed: number;
+  markCurrentCycleAsFinished: () => void;
+  setSecondsPassed: (seconds: number) => void;
+  createNewCycle: (data: CreateCycleData) => void;
+  interruptCurrentCycle: () => void;
+}
