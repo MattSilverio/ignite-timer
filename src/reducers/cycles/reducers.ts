0c548dd3a4eb7c8aa3c8f5d22953f2ef5e11c07f
Matheus Silverio <matheusphillipo@gmail.com>
2023-06-09 12:42:21 -0300

    chore: separated CycleReducer file

---
+++ src/reducers/cycles/reducers.ts
@@ -0,0 +1,37 @@
+import { CyclesState } from "../../interfaces/ICycle";
+import { ActionTypes } from "./actions";
+
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.data],
+        activeCycleId: action.payload.data.id,
+      };
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() };
+          }
+          return cycle;
+        }),
+        activeCycleId: null,
+      };
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() };
+          }
+          return cycle;
+        }),
+        activeCycleId: null,
+      };
+    default:
+      return state;
+  }
+}
